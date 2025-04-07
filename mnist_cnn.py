a663343cbeae3a539262bf3a28a458d7e537ca02
BGKim <bg.kim@sookmyung.ac.kr>
2025-04-07 16:57:04 +0900

    On 2025. 04. 07

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -3,6 +3,7 @@ Gets to 99.25% test accuracy after 12 epochs
 (there is still a lot of margin for parameter tuning).
 16 seconds per epoch on a GRID K520 GPU.
 '''
+# Modified on 2025. 04. 07 by BGKim
 
 from __future__ import print_function
 import keras
@@ -42,8 +43,8 @@ x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
 print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+#print(x_train.shape[0], 'train samples')
+#print(x_test.shape[0], 'test samples')
 
 # convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
@@ -86,7 +87,7 @@ plt.show()
 
 
 ##-- Model Test using Test datasets 
-print()
+#print()
 print("----Actual test for digits----")
 img = Image.open('dataset_test/testimgs/1.png').convert("L")
 img = np.resize(img, (28,28,1))
